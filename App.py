81c3643995406ba993614176d03af1dd9486927f
Pratham Patel <pratham.code10@gmail.com>
2024-12-25 18:59:53 -0600

    Added the view function, need to handle the data

--- App.py
+++ App.py
@@ -1,19 +1,46 @@
 from Employee import Employee
 
-
-print("#####    WELCOME TO SHIFT APP    #####")
-name = input("Please enter your name: ")
+# Functions
 
 def check_role():
     while True:
         role = input("Please tell us your role: ")
         if role == "Manager" or role == "Associate":
             return role
+        
+def getEmployee(name): 
+    for em in Employee.employees:
+        if em.name == name:
+            return em 
+
+
+# Main program
+
+print("#####    WELCOME TO SHIFT APP    #####")
+name = input("Please enter your name: ")
 
 if name not in Employee.employees:
     print("It looks like you are new here!")
     Employee(name, check_role()) # adds the new Employee object
 else:
-    pass
+    print(f"Welcome Back {name}")
+
+employee = getEmployee(name)
+
+while True: 
+    print("""What would you like to do:
+        1. View my Shifts
+        2. Quit
+    """)
+    choice = int(input("> "))
+    if choice == 1:
+        employee.add_shift("Monday", "9:00", "12:00")
+        employee.add_shift("Thursday", "17:00", "21:00")
+        employee.add_shift("Saturday", "12:00", "18:00")
+        employee.display_shifts()
+    elif choice == 2:
+        break
 
 
+
+print("#####    Have a Heavenly Day     #####")
--- Employee.py
+++ Employee.py
@@ -1,3 +1,14 @@
+class Shift: 
+    def __init__(self, day,start_time, end_time):
+        self.day = day
+        self.start_time = start_time
+        self.end_time = end_time
+
+
+    def __str__(self):
+        return f"{self.day}: {self.start_time} - {self.end_time}"
+
+
 class Employee:
 
     employees = []
@@ -6,6 +17,18 @@ class Employee:
         self.name = name
         self.role = role
         Employee.employees.append(self)
+        self.shifts = []
+
 
     def __str__(self):
         return f"Name: {self.name}, Role: {self.role}"
+    
+    def add_shift(self, day, start_time, end_time):
+        self.shifts.append(Shift(day, start_time, end_time))
+    
+    def display_shifts(self):
+        print(f"Shifts for {self.name}")
+        for shift in self.shifts:
+            print(shift)
+    
+
