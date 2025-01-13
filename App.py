fcb5808cfd9dae92584d06786b7adf370c40835d
Pratham Patel <pratham.code10@gmail.com>
2025-01-12 23:10:03 -0600

    Added manager tools and some formatting

--- App.py
+++ App.py
@@ -2,6 +2,54 @@ import database as db
 
 # Only Managers can hire employee for simplicity I am starting with my myself as Manager i.e there is already 1 employee in the database
 
+
+# Functions
+
+
+def manager_tools(manager_name):
+    while True:
+        userChoice = input("""\nWhat would you like to do
+            1. View my shifts
+            2. View other employee shifts
+            3. Add Shift
+            4. Remove Shift
+            5. Quit
+\n> """)
+        print()
+        if userChoice == "1":
+            db.show_shifts(manager_name)
+        elif userChoice == "2":
+            db.show_all()
+            employee_name = input("Whose shift would you like to see (name): ")
+            print()
+            db.show_shifts(employee_name)
+        elif userChoice == "3":
+            pass
+        elif userChoice == "4":
+            pass
+        elif userChoice == "5":
+            break
+        else:
+            print("Incorrect Input\n")
+
+
+def associate_tools(name):
+    while True:
+        userChoice = input("""What would you like to do
+            1. View my shifts
+            2. Quit
+""")
+
+        if userChoice == "1":
+            db.show_shifts(name)
+        elif userChoice == "2":
+            break
+        else:
+            print("Incorrect Input")
+
+
+# Main Program
+
 print("#####    WELCOME TO THE SHIFT APP    #####")
 name = input("Please tell us your name: ")
 
@@ -11,3 +59,13 @@ else:
     exit()
 
 
+employee_role = db.find_employee_role(name)
+
+if employee_role:
+    if employee_role == "Manager":
+        manager_tools(name)
+    else:
+        associate_tools(name)
+
+else:
+    print(f"Employee ({name}) not found")
--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime, timedelta
 
 def create_database():
 
@@ -35,7 +36,7 @@ def add_employee(name, role):
     conn.commit()
     conn.close()
 
-
+# Make sure shifts are valid i.e start time < end time day is the same and shifts dont overlap
 def add_shift(name, start, end):
     conn = sqlite3.connect("employee.db")
     c = conn.cursor()
@@ -58,8 +59,9 @@ def show_all():
     c.execute("SELECT * FROM employees")
 
     employees = c.fetchall()
+    print("ID\tName\t\tRole")
     for employee in employees:
-        print(employee)
+        print(f"{employee[0]}\t{employee[1]}\t\t{employee[2]}")
 
     conn.commit()
     conn.close()
@@ -73,17 +75,32 @@ def show_shifts(name):
     id = find_employee_id(name)
 
     if id:
-        c.execute("SELECT * FROM shifts WHERE (employee_id) = (?)", (id,))
+        c.execute("SELECT start_shift, end_shift FROM shifts WHERE (employee_id) = (?)", (id,))
 
         shifts = c.fetchall()
+
+        print(f"Showing shifts for Employee {id} ({name})")
+        print("Date\t\tStart\t\tEnd")
         for shift in shifts:
-            print(shift)
+            format_shift(shift)
 
     else:
         print(f"Employee ({name}) not found")
 
     conn.commit()
     conn.close()
+
+
+def format_shift(shift_tuple):
+
+    start_time = datetime.strptime(shift_tuple[0], "%Y-%m-%d %H:%M") # 2025-01-01 08:00
+    end_time = datetime.strptime(shift_tuple[1], "%Y-%m-%d %H:%M")   # 2025-01-01 15:00
+
+    date = start_time.strftime("%Y-%m-%d") # 2025-01-01
+    start_time = start_time.strftime("%H:%M") # 08:00
+    end_time = end_time.strftime("%H:%M")   # 15:00
+
+    print(f"{date}\t{start_time}\t\t{end_time}")
 
 
 def find_employee_id(name):
@@ -98,6 +115,22 @@ def find_employee_id(name):
 
     if id:
         return id[0]
+    else:
+        return None
+    
+
+def find_employee_role(name):
+    conn = sqlite3.connect("employee.db")
+    c = conn.cursor()
+
+    c.execute("SELECT employee_role FROM employees WHERE (employee_name) = (?)", (name,))
+    role = c.fetchone()
+
+    conn.commit()
+    conn.close()
+
+    if role:
+        return role[0]
     else:
         return None
     
